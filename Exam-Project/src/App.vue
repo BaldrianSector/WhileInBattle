aa8a792d746f4d5be8eb15fe4cf531a904350294
Baldrian Sector <baldriansector@gmail.com>
2024-06-20 12:05:02 +0200

    Vue + Tailwind

---
+++ Exam-Project/src/App.vue
@@ -0,0 +1,26 @@
+<script setup>
+import BlockSection from './components/BlockSection.vue'
+</script>
+
+<template>
+  <BlockSection id="hero" class="bg-[#161718]" msg="Hero Section" />
+  <BlockSection id="s1" class="bg-[#807164]" msg="Image 1 Section" />
+  <BlockSection id="s1" class="bg-[#57575F]" msg="Image 2 Section" />
+  <BlockSection id="s1" class="bg-[#AA7246]" msg="Quote Section" />
+  <BlockSection id="s1" class="bg-[#4D5C93]" msg="Map Section" />
+</template>
+
+<style scoped>
+.logo {
+  height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+  transition: filter 300ms;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
---
+++ Exam-Project/src/components/BlockSection.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { ref } from 'vue'
+
+defineProps({
+  msg: String,
+})
+
+const count = ref(0)
+</script>
+
+<template>
+  <section class="w-100 h-screen">
+
+    <!-- centered text in the middle of the section -->
+    <div class="flex items-center justify-center h-full">
+      <h1 class="text-4xl text-white">{{ msg }}</h1>
+    </div>
+  
+  </section>
+</template>
+
+<style scoped>
+.read-the-docs {
+  color: #888;
+}
+</style>
